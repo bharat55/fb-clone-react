5dca51e68626cb1ccc514370c686d067341ea027
bharat <bharat.rathor.5074@gmail.com>
2020-12-27 11:21:14 +0530

    Minor changes

--- src/StoryReel.js
+++ src/StoryReel.js
@@ -7,28 +7,33 @@ function StoryReel() {
         <div className="story-reel">
             <Story 
               image={"https://picsum.photos/200/300?grayscale"}
-              profileSrc={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKopUg0IsnfDVuuZaxJZxU93w1a3sDdWgTzw&usqp=CAU"}
-              title="Bharat"
+              profileSrc={"https://scontent.fbho1-2.fna.fbcdn.net/v/t1.0-9/48361746_971677933037326_6753968379427553280_n.jpg?_nc_cat=109&ccb=2&_nc_sid=09cbfe&_nc_ohc=Std7GmOnH4QAX9bivKi&_nc_ht=scontent.fbho1-2.fna&oh=e075290b3488d263a196b2c4108a5b1f&oe=600EA4EF"}
+              title="Harish"
             />
             <Story 
               image={"https://picsum.photos/id/237/200/300"}
-              profileSrc={""}
+              profileSrc={"https://scontent.fbho1-2.fna.fbcdn.net/v/t31.0-1/p200x200/25488241_882818185213719_1709539615799545618_o.jpg?_nc_cat=103&ccb=2&_nc_sid=7206a8&_nc_ohc=CLwKBiHsf2MAX-6I7jd&_nc_ht=scontent.fbho1-2.fna&tp=6&oh=dfd0f0e9ddc55ea3598354ef58b1944c&oe=600EEE6D"}
               title="Ashvin"
             />
             <Story 
               image={"https://images.pexels.com/photos/1089438/pexels-photo-1089438.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"}
-              profileSrc={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKopUg0IsnfDVuuZaxJZxU93w1a3sDdWgTzw&usqp=CAU"}
+              profileSrc={"https://scontent.fbho1-1.fna.fbcdn.net/v/t1.0-1/p200x200/116795456_1391433914387231_7641273651933632733_o.jpg?_nc_cat=107&ccb=2&_nc_sid=7206a8&_nc_ohc=8mCHrEY3U94AX8tPdml&_nc_ht=scontent.fbho1-1.fna&tp=6&oh=2021790d2c467001e6049599a71f5f2e&oe=600DBFC8"}
               title="Ajay"
             />
             <Story 
               image={"https://images.pexels.com/photos/1181263/pexels-photo-1181263.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"}
-              profileSrc={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKopUg0IsnfDVuuZaxJZxU93w1a3sDdWgTzw&usqp=CAU"}
+              profileSrc={"https://scontent.fbho1-1.fna.fbcdn.net/v/t1.0-1/p200x200/36407275_821411784728166_7168006353498144768_n.jpg?_nc_cat=102&ccb=2&_nc_sid=7206a8&_nc_ohc=7--OzNpKQZUAX8r7Cv4&_nc_ht=scontent.fbho1-1.fna&tp=6&oh=44d5def74b525505d698c473272bea3c&oe=600C52EB"}
               title="Ashwin"
             />
             <Story 
               image={"https://images.pexels.com/photos/1181376/pexels-photo-1181376.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"}
-              profileSrc={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKopUg0IsnfDVuuZaxJZxU93w1a3sDdWgTzw&usqp=CAU"}
+              profileSrc={"https://scontent.fbho1-1.fna.fbcdn.net/v/t1.0-1/c0.0.200.200a/p200x200/69439022_1092251864314370_8179329344770932736_o.jpg?_nc_cat=100&ccb=2&_nc_sid=7206a8&_nc_ohc=dtJVrKjooPgAX-1evZw&_nc_ht=scontent.fbho1-1.fna&tp=27&oh=ffad0672151df300c933f829cb2f0f66&oe=600C0419"}
               title="Tarun"
+            />
+            <Story 
+              image={"https://images.unsplash.com/photo-1571910266718-f08412b1ed42?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=634&q=80"}
+              profileSrc={"https://scontent.fbho1-1.fna.fbcdn.net/v/t1.0-9/80838566_2681882028599088_1305049589828550656_n.jpg?_nc_cat=111&ccb=2&_nc_sid=09cbfe&_nc_ohc=TtTcsUQfGLYAX8A-y1G&_nc_ht=scontent.fbho1-1.fna&oh=1de349af1f9817c52421cdf90511723e&oe=600CF3E5"}
+              title="Navin"
             />
         </div>
     )
--- src/Widgets.js
+++ src/Widgets.js
@@ -1,9 +1,54 @@
+import { Avatar, IconButton } from '@material-ui/core'
 import React from 'react'
+import './Widgets.css'
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import EmailIcon from '@material-ui/icons/Email';
+
 
 function Widgets() {
     return (
         <div className="widgets">
-            
+            <div className="developer-info">
+                <img src='IMG_20201116_225414.jpg' alt="Bharat Rathor"/>    
+                <h4>Bharat Rathor (Software Developer) </h4>
+            </div>
+            <div className="developer-skills">
+                <h3 className='tab-heading'> Skills </h3>
+                <ul>
+                    <li>Ruby</li>
+                    <li>Rails</li>
+                    <li>React</li>
+                    <li>javaScript, jQuery</li>
+                    <li>Git</li>
+                    <li>Html, css</li>
+                </ul>
+            </div>
+            <div className="contact-options">
+                <h3 className='tab-heading'>
+                    Contact Details
+                </h3>
+                <div className='contact-option-icons'>
+                    <div className='icon-with-link'>
+                        < IconButton >
+                        <EmailIcon />
+                        </IconButton>
+                       <p>bharat.rathor.5074@gmail.com</p>
+                    </div>
+                    <div className='icon-with-link'>
+                        < IconButton >
+                        <LinkedInIcon />
+                        </IconButton>
+                        <p>www.linkedin.com/in/bharat-rathor-4b519b112</p>
+                    </div>
+                    <div className='icon-with-link'>
+                        < IconButton >
+                        <GitHubIcon />
+                        </IconButton>
+                        <p>https://github.com/bharat55/</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
